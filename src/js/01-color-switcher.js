81acbd831e0b7de9ea388ece8c17699ae5c05320
Vasile Cristian <vasilecristian79@gmail.com>
2023-07-11 23:30:11 +0300

    11 iulie

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+buttonStop.disabled = true;
 
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+
+buttonStart.addEventListener(click, startColor);
+
+buttonStop.addEventListener(click, stopColor);
+
+function startColor() {
+buttonStart.disabled = true;
+buttonStop.disabled = false;
+}
+
+function stopColor() {
+buttonStart.disabled = false;
+buttonStop.disabled = true;
+}
