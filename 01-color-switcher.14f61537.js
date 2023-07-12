17828d05522075ce0a92d1d31d2c051637201fee
vasicris <123592685+vasicris@users.noreply.github.com>
2023-07-12 20:25:53 +0000

    Deploying to gh-pages from @ vasicris/goit-js-hw-09@71a8fa0b911ade3374f2a037da03a105cd49bc33 🚀

---
+++ 01-color-switcher.14f61537.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let d;e.disabled=!0,t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.14f61537.js.map
--- 01-color-switcher.9e6c7ccc.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.disabled=!0,t.addEventListener(click,(function(){t.disabled=!0,e.disabled=!1})),e.addEventListener(click,(function(){t.disabled=!1,e.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.9e6c7ccc.js.map
