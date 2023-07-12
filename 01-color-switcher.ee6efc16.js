d839ec6e29b13ab3db6b7d6db12394e18217bafa
vasicris <123592685+vasicris@users.noreply.github.com>
2023-07-12 20:11:45 +0000

    Deploying to gh-pages from @ vasicris/goit-js-hw-09@9248a908cf60df514ce83f686adf2f5faa444d91 🚀

---
+++ 01-color-switcher.ee6efc16.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.disabled=!0,t.addEventListener(click,(function(){t.disabled=!0,e.disabled=!1})),e.addEventListener(click,(function(){t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.ee6efc16.js.map
