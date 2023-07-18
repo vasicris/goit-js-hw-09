2c7880032d4489de6ad89f12b783a2f62745f622
Vasile Cristian <vasilecristian79@gmail.com>
2023-07-18 21:33:21 +0300

    18 iulie

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,7 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
-
-flatpickr(datePicker, options);
+import Notiflix from 'notiflix';
 
 const datePicker = document.querySelector('#datetime-picker');
 const btnStart = document.querySelector('button[data-start]');
@@ -9,7 +8,6 @@ const days = document.querySelector('span[data-days]');
 const hours = document.querySelector('span[data-hours]');
 const minutes = document.querySelector('span[data-minutes]');
 const seconds = document.querySelector('span[data-seconds]');
-const timer = document.querySelector('.timer');
 
 const options = {
     enableTime: true,
@@ -22,9 +20,11 @@ const options = {
         Notiflix.Notify.failure('Please choose a date in the future');
         return;
       }
-      startBtn.disabled = false;
+      btnStart.disabled = false;
     },
   };
+
+  flatpickr(datePicker, options);
 
 function convertMs(ms) {
     // Number of milliseconds per unit of time
@@ -45,17 +45,25 @@ function convertMs(ms) {
     return { days, hours, minutes, seconds };
   }
 
-  function addLeadingZero(value) {
-    for (let key in value) {
-      value[key] = String(value[key]).padStart(2, '0');
-    }
-    return value;
-  }
+    function addLeadingZero(value) {
+        return String(value).padStart(2, '0');
+      }
+    
+let dateSelected = null;
+
   function countDownTime() {
-    ms = dateSelected - new Date();
-    if (ms < 999) {
-      clearInterval(intervalId);
-    }
+   const ms = dateSelected - new Date();
+    if (ms >= 0) {
+        let timeObject = convertMs(ms);
+        days.textContent = addLeadingZero(timeObject.days);
+        hours.textContent = addLeadingZero(timeObject.hours);
+        minutes.textContent = addLeadingZero(timeObject.minutes);
+        seconds.textContent = addLeadingZero(timeObject.seconds);
+      } else {
+        Notiflix.Notify.success('Countdown finished');
+        clearInterval(intervalId);
+      }
+
     let convertedValues = convertMs(ms);
     addLeadingZero(convertedValues);
     days.textContent = convertedValues.days;
@@ -67,6 +75,3 @@ function convertMs(ms) {
   btnStart.addEventListener('click', () => {
     intervalId = setInterval(countDownTime, 1000);
   });
-
-
-
